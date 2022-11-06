49284a4f00a01ab777ddec1c96cca56d26edf488
Evan Nevermore <InfiniteInsight@gmail.com>
2022-11-06 14:34:28 -0500

    add line 24 to call function to check for local AWS Credentials

--- main.py
+++ main.py
@@ -14,13 +14,14 @@ parser.add_argument('--method', '-m', type=str, help='the scan type', required=T
 parser.add_argument('--aws_profile', '-p', type=str, help='the aws profile for the access keys', required=True)
 parser.add_argument('--scanner_role', '-r', type=str, help='the aws scanner\'s role name', required=True)
 parser.add_argument('--last_modified', '-l', type=int, default=1,
-                    help='the number of days to scan since the file was last modified')
+                    help='the number of days to scan since the file was last modified (scan files this many days old)')
 
 args = parser.parse_args()
 
 if __name__ == "__main__":
     utils.print_banner()
     utils.prerequisite_checks('trufflehog3')
+    utils.check_for_local_aws_credentials()
 
     if args.method.lower() == 'internal':
         logger.info(f"[+] Scanning for files which last modified since {utils.get_modified_date(args.last_modified)}")
