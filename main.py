325cd964a37e983b4e50ba69dc70ad65a128d006
Evan Nevermore <InfiniteInsight@gmail.com>
2022-11-19 23:59:28 -0500

    colorized terminal text, added utils.check_for_local_aws_credentials, added a call to utils.create_downloads_folder, added 'aws' to the prerequisite check alongside trufflehog3

--- main.py
+++ main.py
@@ -19,16 +19,17 @@ parser.add_argument('--last_modified', '-l', type=int, default=1,
 args = parser.parse_args()
 
 if __name__ == "__main__":
-    colorize = utils.Colors()
+    colors = utils.Colors()
     utils.print_banner()
     utils.check_for_local_aws_credentials()
-    utils.prerequisite_checks('trufflehog3')
+    utils.create_downloads_folder()
+    utils.prerequisite_checks('trufflehog3','aws')
     
 
     if args.method.lower() == 'internal':
-        print(colorize.WARNING)
+        print(colors.WARNING)
         logger.info(f"[+] Scanning for files which last modified since {utils.get_modified_date(args.last_modified)}")
-        print(colorize.ENDC)
+        print(colors.ENDC, end="")
         scripts.scan_internal_accounts(args.aws_profile, args.scanner_role, args.last_modified)
     else:
-        print(colorize.LIGHTRED,'[!] Not a valid action - please choose the method \"internal\"', colorize.ENDC)
+        print(colors.LIGHTRED,'[!] Not a valid action - please choose the method \"internal\"', colors.ENDC)
